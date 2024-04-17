4648380fb5da070a40bb94e75de2276993a592bf
Carsten Blank <blank@data-cybernetics.com>
2024-04-17 20:10:09 +0200

    Small adjustments

--- src/q_alchemy/qiskit_integration.py
+++ src/q_alchemy/qiskit_integration.py
@@ -3,12 +3,13 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #     http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import datetime
 import logging
 import os
 import hashlib
@@ -124,7 +125,7 @@ class QAlchemyInitialize(Instruction):
         job_timeout = self.opt_params.job_completion_timeout_sec * 1000
         job = (
             Job(self.client)
-            .create(name='Execute Transformation')
+            .create(name=f'Execute Transformation ({datetime.datetime.now()})')
             .select_processing(processing_step='convert_circuit_layers')
             .configure_parameters(
                 min_fidelity=1.0 - self.opt_params.max_fidelity_loss,
