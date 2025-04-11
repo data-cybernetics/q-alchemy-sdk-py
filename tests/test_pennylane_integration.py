74bf1aa5d184bef2e259a913451b164ccd3c787b
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2025-04-11 13:41:51 -0300

    Pennylane integration test

--- tests/test_pennylane_integration.py
+++ tests/test_pennylane_integration.py
@@ -5,7 +5,7 @@ import pennylane as qml
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 
-from q_alchemy.parser.qasm_pennylane import from_qasm
+from q_alchemy.pennylane_integration import QAlchemyStatePreparation, OptParams
 
 
 class TestPennyLaneIntegration(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestPennyLaneIntegration(unittest.TestCase):
         # This method will be called after each test
         pass
 
-    def test_pennylane_vs_qiskit(self):
+    def test_fixed_complex(self):
 
         with open("data/test.qasm", "r") as f:
             qasm = f.read()
@@ -29,14 +29,69 @@ class TestPennyLaneIntegration(unittest.TestCase):
         dev = qml.device('default.qubit')
 
         @qml.qnode(dev)
-        def circuit_pennylane(q):
-            from_qasm(q)
+        def circuit_pennylane(state):
+            QAlchemyStatePreparation(
+                state,
+                wires=range(qc.num_qubits),
+                opt_params=OptParams(
+                    api_key="<your api key>"
+                )
+            )
             return qml.state()
 
-        state_pennylane = circuit_pennylane(qasm)
+        state_pennylane = circuit_pennylane(state_qiskit)
 
         self.assertLessEqual(np.linalg.norm(state_qiskit - state_pennylane), 1e-13)
         self.assertLessEqual(1 - abs(np.vdot(state_qiskit, state_pennylane))**2, 1e-13)
+
+
+    def test_rnd_real(self):
+
+        n_qubits = 4
+        state_vector = np.random.rand(2**n_qubits)
+        state_vector = state_vector / np.linalg.norm(state_vector)
+
+        dev = qml.device('default.qubit')
+
+        @qml.qnode(dev)
+        def circuit_pennylane(state):
+            QAlchemyStatePreparation(
+                state,
+                wires=range(n_qubits),
+                opt_params=OptParams(
+                    api_key="<your api key>"
+                )
+            )
+            return qml.state()
+
+        state_pennylane = circuit_pennylane(state_vector)
+
+        self.assertLessEqual(np.linalg.norm(state_vector - state_pennylane), 1e-13)
+        self.assertLessEqual(1 - abs(np.vdot(state_vector, state_pennylane))**2, 1e-13)
+
+    def test_rnd_complex(self):
+
+        n_qubits = 4
+        state_vector = np.random.rand(2**n_qubits) + np.random.rand(2**n_qubits) * 1j
+        state_vector = state_vector / np.linalg.norm(state_vector)
+
+        dev = qml.device('default.qubit')
+
+        @qml.qnode(dev)
+        def circuit_pennylane(state):
+            QAlchemyStatePreparation(
+                state,
+                wires=range(n_qubits),
+                opt_params=OptParams(
+                    api_key="<your api key>"
+                )
+            )
+            return qml.state()
+
+        state_pennylane = circuit_pennylane(state_vector)
+
+        self.assertLessEqual(np.linalg.norm(state_vector - state_pennylane), 1e-13)
+        self.assertLessEqual(1 - abs(np.vdot(state_vector, state_pennylane))**2, 1e-13)
 
 
 if __name__ == '__main__':
