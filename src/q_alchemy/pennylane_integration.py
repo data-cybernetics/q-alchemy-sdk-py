a6205fd969c129ab901ab382f22d1315cf6cb707
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2025-04-22 16:48:07 -0300

    Qiskit and Qclib compatible normalization

--- src/q_alchemy/pennylane_integration.py
+++ src/q_alchemy/pennylane_integration.py
@@ -14,14 +14,18 @@ from typing import Optional, Union
 from scipy.sparse import csr_matrix
 
 import pennylane as qml
+import numpy as np
+from pennylane import math
 from pennylane.ops.qubit.state_preparation import StatePrep
 from pennylane.operation import Operation, Operator
 from pennylane.typing import TensorLike
-from pennylane.wires import WiresLike
+from pennylane.wires import Wires, WiresLike
 
 from q_alchemy.initialize import q_alchemy_as_qasm, OptParams
 
-LOG = logging.getLogger(__name__)
+# Normalization precision required for compatibility with Qiskit and qclib state preparation.
+ATOL = 1e-10
+RTOL = 1e-9
 
 class AmplitudeEmbedding(StatePrep):
     # pylint: disable=too-many-arguments,too-many-positional-arguments
@@ -76,8 +80,85 @@ class AmplitudeEmbedding(StatePrep):
         [QAlchemyStatePreparation(tensor([1, 0, 0, 0], requires_grad=True), wires=[0, 1])]
 
         """
-
         return [QAlchemyStatePreparation(state, wires, id=None, **kwargs)]
+
+    @staticmethod
+    def _preprocess(state, wires, pad_with, normalize, validate_norm):
+        """Validate and pre-process inputs as follows:
+
+        * If state is batched, the processing that follows is applied to each state set in the batch.
+        * Check that the state tensor is one-dimensional.
+        * If pad_with is None, check that the last dimension of the state tensor
+          has length :math:`2^n` where :math:`n` is the number of qubits. Else check that the
+          last dimension of the state tensor is not larger than :math:`2^n` and pad state
+          with value if necessary.
+        * If normalize is false, check that last dimension of state is normalised to one. Else, normalise the
+          state tensor.
+        """
+        if isinstance(state, (list, tuple)):
+            state = math.array(state)
+
+        # Promote from `float32` to `float64` to ensure normalization meets the required precision.
+        if "float32" in str(state.dtype):
+            state = state.astype(np.float64, copy=False)
+        if "complex64" in str(state.dtype):
+            state = state.astype(np.complex128, copy=False)
+
+        shape = math.shape(state)
+
+        # check shape
+        if len(shape) not in (1, 2):
+            raise ValueError(
+                f"State must be a one-dimensional tensor, or two-dimensional with batching; got shape {shape}."
+            )
+
+        n_states = shape[-1]
+        dim = 2 ** len(Wires(wires))
+        if pad_with is None and n_states != dim:
+            raise ValueError(
+                f"State must be of length {dim}; got length {n_states}. "
+                f"Use the 'pad_with' argument for automated padding."
+            )
+
+        if pad_with is not None:
+            normalize = True
+            if n_states > dim:
+                raise ValueError(
+                    f"Input state must be of length {dim} or "
+                    f"smaller to be padded; got length {n_states}."
+                )
+
+            # pad
+            if n_states < dim:
+                padding = [pad_with] * (dim - n_states)
+                if len(shape) > 1:
+                    padding = [padding] * shape[0]
+                padding = math.convert_like(padding, state)
+                state = math.hstack([state, padding])
+
+        if not validate_norm:
+            return state
+
+        # normalize
+        if "int" in str(state.dtype):
+            state = math.cast_like(state, 0.0)
+
+        norm = math.linalg.norm(state, axis=-1)
+
+        if math.is_abstract(norm):
+            if normalize:
+                state = state / math.reshape(norm, (*shape[:-1], 1))
+
+        elif not math.allclose(norm, 1.0, atol=ATOL, rtol=RTOL):
+            if normalize:
+                state = state / math.reshape(norm, (*shape[:-1], 1))
+            else:
+                raise ValueError(
+                    f"The state must be a vector of norm 1.0; got norm {norm}. "
+                    "Use 'normalize=True' to automatically normalize."
+                )
+
+        return state
 
 class QAlchemyStatePreparation(Operation):
     def __init__(self, state_vector, wires, id=None, **kwargs):
@@ -115,7 +196,7 @@ class QAlchemyStatePreparation(Operation):
 
             if not qml.math.is_abstract(state):
                 norm = qml.math.sum(qml.math.abs(state) ** 2)
-                if not qml.math.allclose(norm, 1.0, atol=1e-3):
+                if not qml.math.allclose(norm, 1.0, atol=ATOL, rtol=RTOL):
                     raise ValueError(
                         f"State vectors have to be of norm 1.0, vector {i} has norm {norm}"
                     )
