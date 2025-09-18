1d74f095f535fc508c2bacd525f597668aba2a35
keith.ng <keith.ng@data-cybernetics.com>
2025-09-18 16:12:28 +0200

    Fix imports breaking if user did not install all optionals
    Not sure this is the idiomatic way of handling this, but this will at least make testing easier.

--- src/q_alchemy/__init__.py
+++ src/q_alchemy/__init__.py
@@ -1,3 +1,9 @@
 from .initialize import q_alchemy_as_qasm
-from .qiskit_integration import QAlchemyInitialize
-from .pennylane_integration import QAlchemyStatePreparation
+try: # should fail silently if user has not installed optional dependencies
+    from .qiskit_integration import QAlchemyInitialize
+except ImportError:
+    pass
+try:
+    from .pennylane_integration import QAlchemyStatePreparation
+except ImportError:
+    pass
