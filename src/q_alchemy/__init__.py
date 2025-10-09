a0f299c404a0862797f1a781fe81e9a2b9b0744a
keith.ng <keith.ng@data-cybernetics.com>
2025-10-09 10:26:25 +0200

    Add INFO-level message if import "fails" due to missing optional, instead of failing silently.
    Could be lowered to DEBUG level, if necessary.

--- src/q_alchemy/__init__.py
+++ src/q_alchemy/__init__.py
@@ -1,9 +1,11 @@
+import logging
+LOG = logging.getLogger(__name__)
 from .initialize import q_alchemy_as_qasm
 try: # should fail silently if user has not installed optional dependencies
     from .qiskit_integration import QAlchemyInitialize
 except ImportError:
-    pass
+    LOG.info("qiskit_integration module not available")
 try:
     from .pennylane_integration import QAlchemyStatePreparation
 except ImportError:
-    pass
+    LOG.info("pennylane_integration module not available")
