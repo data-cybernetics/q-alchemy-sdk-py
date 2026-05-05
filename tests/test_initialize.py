6bd0c59689d2058bbf0d2f261334c40bf3b24947
keith.ng <keith.ng@data-cybernetics.com>
2026-05-05 12:40:01 +0200

    Remove lingering env-dev's.

--- tests/test_initialize.py
+++ tests/test_initialize.py
@@ -10,7 +10,7 @@ from q_alchemy.initialize import OptParams, q_alchemy_as_qasm_parallel_states, I
 
 from dotenv import load_dotenv
 
-load_dotenv("../.env-dev") # the 'assert' was causing the import to fail during test discovery.
+load_dotenv("../.env") # the 'assert' was causing the import to fail during test discovery.
 
 class InitializeTestCase(unittest.TestCase):
     def test_batch(self):
